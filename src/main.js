5a830d8bca0eccddfc9d8682c295921ef1bec910
Frederick Arbianto <frederick.arbianto@yogyagroup.com>
2019-11-17 02:17:52 +0700

    Replace vue-http with axios

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
+import axios from "axios";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
+Vue.prototype.$http = axios;
 
 new Vue({
   router,
