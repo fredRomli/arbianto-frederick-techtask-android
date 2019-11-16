555a07357919b6beec30cef3c9e0f98dafb19225
Frederick Arbianto <frederick.arbianto@yogyagroup.com>
2019-11-17 04:01:25 +0700

    Add proxy to API call for local dev server

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
+  devServer: {
+    proxy: {
+      "/ingredients": {
+        target: "https://a61d556b-57ca-423f-8706-2e8dec75d714.mock.pstmn.io"
+      },
+      "/recipes": {
+        target: "https://a61d556b-57ca-423f-8706-2e8dec75d714.mock.pstmn.io"
+      }
+    }
+  },
   transpileDependencies: ["vuetify"]
 };
