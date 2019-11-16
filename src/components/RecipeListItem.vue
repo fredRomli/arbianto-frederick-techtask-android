37721cb679e43bf1416d6bc804ec3af123221483
Frederick Arbianto <frederick.arbianto@yogyagroup.com>
2019-11-17 04:16:46 +0700

    Create component for displaying recipe

---
+++ src/components/RecipeListItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-list-item disabled two-line>
+    <v-list-item-content>
+      <v-list-item-title>{{ recipe.title }}</v-list-item-title>
+      <v-list-item-subtitle>{{ ingredients }}</v-list-item-subtitle>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  name: "RecipeListItem",
+  props: {
+    recipe: {
+      type: Object,
+      required: true
+    }
+  },
+  data: function() {
+    return {
+      recipeData: this.recipe
+    };
+  },
+  computed: {
+    ingredients: function() {
+      return this.recipeData ? this.recipeData.ingredients.join(", ") : "";
+    }
+  }
+};
+</script>
